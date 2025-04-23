2c7a7060d7eca16fcb767e72a85093878ba0a5f2
Tobias Moreno <tobias.moreno@techforb.com>
2025-04-23 10:01:29 -0300

    CHG: se modifica CardComponent y se le agrega el input q recibe

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,30 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { Building } from './building.model';
 import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  standalone: true,
   imports: [CurrencyPipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  building: Building = {
-    id: 1,
-    name: 'Departamento Moderno',
-    type: {
-      id: 1,
-      name: 'Departamento',
-    },
-    price: 800,
-    beds: 2,
-    bathrooms: 1,
-    size: 60,
-    direction: 'Av. Los álamos 1440 Rosario',
-    image: 'assets/images/carrusel-dept.jpeg',
-  };
-
+  building = input.required<Building>();
+  
   icons = {
     bed: 'assets/images/bed.svg',
     bath: 'assets/images/bathroom.svg',
