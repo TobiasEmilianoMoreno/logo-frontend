481136845378dcbf5fb3cb684fe800c087bf6f61
Tobias Moreno <tobias.moreno@techforb.com>
2025-04-23 16:03:45 -0300

    DEL: se eliminan rutas de prueba

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,4 @@
 import { Routes } from '@angular/router';
-import { SearchBarComponent } from './features/hero-section/search-bar/search-bar.component';
 
 export const routes: Routes = [
-    /* {
-        path: 'search',
-        component: SearchBarComponent
-    },
-    {
-        path: '**',
-        redirectTo: 'search'
-    } */
 ];
