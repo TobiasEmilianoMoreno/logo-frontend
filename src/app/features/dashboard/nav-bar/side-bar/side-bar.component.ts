8f9db8aaaffebb067fb0851e99dda24fa8c9441d
Tobias Moreno <tobias.moreno@techforb.com>
2025-04-24 14:30:15 -0300

    ADD: se agrega sideBar y navBar

---
+++ src/app/features/dashboard/nav-bar/side-bar/side-bar.component.ts
@@ -0,0 +1,28 @@
+import { Component, computed, inject } from '@angular/core';
+import { MenuItem } from '../menuItem.model';
+import { HttpClient } from '@angular/common/http';
+import { SideBarService } from '../../../../shared/services/sider-bar.service';
+
+@Component({
+  selector: 'app-side-bar',
+  imports: [],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.scss',
+})
+export class SideBarComponent {
+  menuItems: MenuItem[] = [];
+  private http = inject(HttpClient);
+  private sidebarService = inject(SideBarService);
+
+  isOpen = computed(() => this.sidebarService.isOpen());
+
+  ngOnInit(): void {
+    this.http
+      .get<MenuItem[]>('assets/dashboard/menu-items.json')
+      .subscribe((data) => (this.menuItems = data));
+  }
+
+  onMenuClick() {
+    this.sidebarService.close();
+  }
+}
