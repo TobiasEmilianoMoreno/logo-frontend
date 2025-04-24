31a03fc8119c3aa5d8fe7c7cbd3cef4a9c3bbff0
Tobias Moreno <tobias.moreno@techforb.com>
2025-04-24 17:33:10 -0300

    CHG: se modificó el nav para tener unicamente pixeles

---
+++ src/app/features/dashboard/panel/panel.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { DashboardCardComponent } from '../dashboard-card/dashboard-card.component';
+
+@Component({
+  selector: 'app-panel',
+  imports: [DashboardCardComponent],
+  templateUrl: './panel.component.html',
+  styleUrl: './panel.component.scss',
+})
+export class PanelComponent {
+  cards = [
+    {
+      title: 'Total Revenue',
+      value: 45890,
+      actionIcon: 'assets/dashboard/currency-usd.svg',
+      actionAlt: 'dollar',
+    },
+    {
+      title: 'Total Visitor',
+      value: '2456',
+      actionIcon: 'assets/dashboard/account-group-outline.svg',
+      actionAlt: 'users',
+    },
+    {
+      title: 'Total Properties',
+      value: '358',
+      actionIcon: 'assets/dashboard/home-city-outline.svg',
+      actionAlt: 'home',
+    },
+    {
+      title: 'Properties for Sell',
+      value: '243',
+      actionIcon: 'assets/dashboard/home-lightning-bolt-outline.svg',
+      actionAlt: 'sale',
+    },
+    {
+      title: 'Properties for Rent',
+      value: '115',
+      actionIcon: 'assets/dashboard/home-clock-outline.svg',
+      actionAlt: 'sale',
+    },
+  ];
+}
